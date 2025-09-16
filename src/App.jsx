0bc69f555aed33377f5597d0685983c79e41f8a3
tihitina-k <tihitinak19@gmail.com>
2025-09-16 21:15:13 +0300

    amazon header

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import Header from './Components/Header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+<Header/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import classes from "./Header.module.css";
+import { FaShoppingCart } from "react-icons/fa";
+import { SlLocationPin } from "react-icons/sl";
+import { BsSearch } from "react-icons/bs";
+import { BiCart } from "react-icons/bi";
+
+const Header = () => {
+  return (
+    <>
+      <div className={classes.Header_Container}>
+        {/* Left Section: Logo */}
+        <div className={classes.logo_Container}>
+          <a href="/">
+            <img
+              src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+              alt="amazon logo"
+            />
+          </a>
+        </div>
+
+        {/* Delivery Info */}
+        <div className={classes.delivery}>
+          <span>
+            <SlLocationPin />
+          </span>
+          <div>
+            <p>Delivered to</p>
+            <span>Ethiopia</span>
+          </div>
+        </div>
+
+        {/* Search */}
+        <div className={classes.search}>
+          <select>
+            <option value="">All</option>
+          </select>
+          <input type="text" placeholder="Search..." />
+          <BsSearch size={25} />
+        </div>
+
+        {/* Language */}
+        <div className={classes.language}>
+          <a href="">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/4692/4692307.png"
+              alt="flag"
+            />
+            <select name="" id="">
+              <option value="">ENG</option>
+            </select>
+          </a>
+        </div>
+
+        {/* Right Section */}
+        <div className={classes.rightSection}>
+          {/* Sign In */}
+          <a href="">
+            <div>
+              <p>Sign In</p>
+              <span>Account & Lists</span>
+            </div>
+          </a>
+
+          {/* Orders */}
+          <a href="">
+            <p>Returns</p>
+            <span>& Orders</span>
+          </a>
+
+          {/* Cart */}
+          <a href="" className={classes.cart}>
+            <BiCart size={35} />
+            <span>0</span>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
